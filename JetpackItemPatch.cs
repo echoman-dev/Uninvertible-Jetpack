e726c48e1a08758ea813882498651f06e93ee405
echoman-dev <jancso.jonatan@gmail.com>
2024-01-25 20:56:38 +0100

    Add project files.

---
+++ JetpackItemPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace Uninvertible_Jetpack
+{
+    [HarmonyPatch(typeof(JetpackItem))]
+    internal class JetpackItemPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(JetpackItem.DiscardItem))]
+        public static void DiscardItem(ref JetpackItem __instance)
+        {
+            __instance.isBeingUsed = false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Uninvertible_Jetpack
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInProcess("Lethal Company.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "io.github.echoman-dev.uninvertible-jetpack";
+        private const string modName = "Lethal Company Uninvertible Jetpacks";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        internal static ManualLogSource log;
+
+        void Awake()
+        {
+            log = Logger;
+
+            log.LogInfo($"{modName} v{modVersion} loaded.");
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
